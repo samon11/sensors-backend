f102d1c126a22f33aa87bc63fbc7f19a9f12a84c
NathanClapp <45773714+NathanClapp@users.noreply.github.com>
2018-12-15 12:49:46 -0700

    Update Dict.py

--- python/Dict.py
+++ python/Dict.py
@@ -10,7 +10,7 @@ def parse(data):
     newString = data + ","
     dict = {'light':0.0,'temp':0.0,'temp_barom':0.0,'pressure':0.0,'humidity':0.0}
     # r refers to a raw Python string
-    # \:(.*?)\, is just a complicated way of saying, "Look between the colon and the comma!"
+    # \:(.*?)\, is just a complicated way of saying, "Look between each colon/comma delimiting pair!"
     strData = re.findall(r"\:(.*?)\,",newString)
     while i <= len(dict):
         if i == 0:
