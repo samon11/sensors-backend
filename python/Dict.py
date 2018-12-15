e3132d46770019bf5f574486afd51502aa4ba063
NathanClapp <45773714+NathanClapp@users.noreply.github.com>
2018-12-15 12:48:40 -0700

    Update Dict.py

--- python/Dict.py
+++ python/Dict.py
@@ -1,12 +1,16 @@
 #Grab input string
 data = "LUM:34.29,tF:0.01,tF_B:68.39,Pa:1829.083,Mt:0.65"
 
+
 import re
 
 def parse(data):
     i = 0
+    # This allows the findall function to recognize the Mt: data.
     newString = data + ","
     dict = {'light':0.0,'temp':0.0,'temp_barom':0.0,'pressure':0.0,'humidity':0.0}
+    # r refers to a raw Python string
+    # \:(.*?)\, is just a complicated way of saying, "Look between the colon and the comma!"
     strData = re.findall(r"\:(.*?)\,",newString)
     while i <= len(dict):
         if i == 0:
