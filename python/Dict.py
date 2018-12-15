71fdc3182939b314e278b8cc8ca6e2abc323d02b
NathanClapp <45773714+NathanClapp@users.noreply.github.com>
2018-12-15 12:39:46 -0700

    Add files via upload

---
+++ python/Dict.py
@@ -0,0 +1,25 @@
+#Grab input string
+data = "LUM:34.29,tF:0.01,tF_B:68.39,Pa:1829.083,Mt:0.65"
+
+import re
+
+def parse(data):
+    i = 0
+    newString = data + ","
+    dict = {'light':0.0,'temp':0.0,'temp_barom':0.0,'pressure':0.0,'humidity':0.0}
+    strData = re.findall(r"\:(.*?)\,",newString)
+    while i <= len(dict):
+        if i == 0:
+            dict['light'] = strData[i]
+        if i == 1:
+            dict['temp'] = strData[i]
+        if i == 2:
+            dict['temp_barom'] = strData[i]
+        if i == 3:
+            dict['pressure'] = strData[i]
+        if i == 4:
+            dict['humidity'] = strData[i]
+            return dict
+        i = i + 1
+
+print(parse(data))
