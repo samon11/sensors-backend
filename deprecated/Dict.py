93a50af99ed0a6168834152e9182c28617c1c546
Michael Samon <michael.samon11@gmail.com>
2019-01-23 10:48:39 -0700

    deprecated Dict.py

--- deprecated/Dict.py
+++ deprecated/Dict.py
@@ -8,16 +8,20 @@ def parse(data):
     # r refers to a raw Python string
     # \:(.*?)\, is just a complicated way of saying, "Look between each colon/comma delimiting pair!"
     strData = re.findall(r"\:(.*?)\,",newString)
+    print(strData[0])
+    print(strData[1])
     while i <= len(dict):
         if i == 0:
             dict['light'] = float(strData[i])
         if i == 1:
             dict['temp'] = float(strData[i])
-        if i == 2:
-            dict['temp_barom'] = float(strData[i])
-        if i == 3:
-            dict['pressure'] = float(strData[i])
-        if i == 4:
-            dict['humidity'] = float(strData[i][:-1]) # do not include trailing ","
-            return dict
+        # if i == 2:
+        #     dict['temp_barom'] = float(strData[i])
+        # if i == 3:
+        #     dict['pressure'] = float(strData[i])
+        # if i == 4:
+        #     dict['humidity'] = float(strData[i][:-1]) # do not include trailing ","
         i = i + 1
+        return dict
+
+print(parse("Variable 1: 6, Variable 2: 12"))
