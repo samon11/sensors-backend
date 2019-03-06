2845213aee86038ca57f4a882be481b757e99e77
Michael Samon <mjsamon@protonmail.com>
2019-03-06 07:56:51 -0700

    save data to postgres db

--- api/api_srv.py
+++ api/api_srv.py
@@ -1,7 +1,9 @@
 from flask import Flask, request
-import re
+from database import session, Users, Airdata
 import socket
-import json 
+import json
+from datetime import datetime
+
 
 app = Flask(__name__)
 
@@ -11,26 +13,46 @@ def start():
     return "Host ID: {}".format(host)
 
 
+def insert_data(request):
+    """Parse request json data and insert into db"""
+    raw_data = request.get_data(as_text=True, cache=False)
+    raw_data = json.loads(raw_data)
+
+    # remove any whitespace and parse to dictionary
+    sensor_data = raw_data['data'].replace(" ", "")
+    sensor_data = (dict(x.split(':') for x in sensor_data.split(',')))
+    
+    # convert to floats and remove chars after '_'
+    sensor_data = {k:float(v[:v.index('_')]) for k, v in sensor_data.items() if k != "minSinceStart"}
+    
+    print(sensor_data)
+
+    # this timestamp is the time at which the sensor
+    # pushed the data
+    timestamp = raw_data['published_at']
+
+    particle_id = raw_data['coreid']
+
+    # get the sensor id from the users table if it exists
+    sensor_owner = session.query(Users).filter_by(particle_str=particle_id).first()
+
+    data_insert = Airdata(
+            device_time=timestamp,
+            temperature_C=sensor_data["Temp"],
+            pressure_hPa=sensor_data["Hum"],
+            humidity_perc=sensor_data["Press"],
+            user=sensor_owner
+    )
+
+    session.add(data_insert)
+    session.commit()
+
+
 @app.route('/indata', methods=['POST'])
-def insert_data():
+def indata():
     if request.method == "POST":
-        raw_data = request.get_data(as_text=True, cache=False)
-
-        raw_data = json.loads(raw_data)
-
-        # remove any whitespace and parse to dictionary
-        sensor_data = raw_data['data'].replace(" ", "")  
-        sensor_data = (dict(x.split(':') for x in sensor_data.split(',')))
-
-        # this timestamp is the time at which the sensor
-        # pushed the data, not when the csv is made
-        timestamp = raw_data['published_at']
-        sensor_id = raw_data['coreid']
-
-        print(sensor_id)
-        return sensor_id
-
-        # TODO - create and manage csv data
+        insert_data(request)
+        return "200-OK"
 
 
 if __name__ == '__main__':
